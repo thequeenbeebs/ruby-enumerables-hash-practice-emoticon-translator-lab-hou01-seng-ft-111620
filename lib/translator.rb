257bc3214027bb3fa610171a7a49879c3ad9d973
Blaire Baker <bakerblaire@gmail.com>
2020-10-27 15:56:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,11 +5,12 @@ require "pry"
 def load_library(file)
   # code goes here
   emoticons = YAML.load_file(file)
-  binding.pry
+  
   emoticons.each do |emotion, array_of_emoticons|
-    emoticons[emotion] =
-    {:english => array_of_emoticons[0],
+    emoticons[emotion] = 
+    {:english => array_of_emoticons[0], 
     :japanese => array_of_emoticons[1]}
+    binding.pry
   end
 end
 
