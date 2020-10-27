5edc23f79bbaf9cd8ab27d79a33000659cc743c3
Blaire Baker <bakerblaire@gmail.com>
2020-10-27 16:11:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,6 +14,7 @@ end
 def get_japanese_emoticon (file, emoticon)
   file = load_library(file)
   file.each do |emotion, hash_of_languages|
+    binding.pry
     if emoticon == emotion[hash_of_languages]
       puts emotion
     end
