b1fc5a0dcbd835eb07c277239b3610d8d9bc1686
Blaire Baker <bakerblaire@gmail.com>
2020-10-27 16:21:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ def get_japanese_emoticon (file, emoticon)
   file = load_library(file)
   file.each do |emotion, hash_of_languages|
     if emoticon == hash_of_languages[:english]
-      file[hash_of_languages][:japanese]
+      return file[hash_of_languages][:japanese]
     else
       return "Sorry, that emoticon was not found"
     end
